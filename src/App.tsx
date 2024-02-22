b44e1463e3b658bb1157f4c7e3dabcc84fa70a43
Sutan Arief <sutanarief21@gmail.com>
2024-02-23 01:55:08 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Body from "./components/body";
+import Navbar from "./components/navbar";
+
+function App() {
+
+  return (
+    <div className="bg-[#CECECE]">
+      <Navbar />
+      <Body />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/body.tsx
@@ -0,0 +1,14 @@
+import content1 from "../assets/images/content1.png";
+
+function Body() {
+
+  return (
+    <>
+      <div className="p-20">
+        <img src={content1} alt="content-1" />
+      </div>
+    </>
+  )
+}
+
+export default Body
