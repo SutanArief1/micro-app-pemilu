aca964f002a644876d7fd7f0e98103b92958a8a9
Sutan Arief <sutanarief21@gmail.com>
2024-03-04 03:47:23 +0700

    register

--- src/components/modalRegister/index.tsx
+++ src/components/modalRegister/index.tsx
@@ -1,6 +1,19 @@
-import { NavLink } from "react-router-dom"
+import { ChangeEvent, useState } from "react"
+import { NavLink, useNavigate } from "react-router-dom"
 
 function ModalRegister() {
+  const [fullname, setFullname] = useState('')
+  const [alamat, setAlamat] = useState('')
+  const [jenisKelamin, setJenisKelamin] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  const handleFullnameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFullname(event.target.value)
+  }
+
+
 
   return (
     <>
@@ -11,24 +24,24 @@ function ModalRegister() {
           </div>
           <div className="flex flex-col gap-5">
             <div className="flex flex-col">
-              <label htmlFor="username" className="font-semibold">Fullname</label>
+              <label htmlFor="fullname" className="font-semibold">Fullname</label>
+              <input type="text" id="fullname" name="fullname" className="border-2 border-black h-11 rounded-lg" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="address" className="font-semibold">Alamat</label>
+              <input type="text" id="address" name="address" className="border-2 border-black h-11 rounded-lg" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="gender" className="font-semibold">Jenis Kelamin</label>
+              <input type="text" id="gender" name="gender" className="border-2 border-black h-11 rounded-lg" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="username" className="font-semibold">Username</label>
               <input type="text" id="username" name="username" className="border-2 border-black h-11 rounded-lg" />
             </div>
             <div className="flex flex-col">
-              <label htmlFor="password" className="font-semibold">Alamat</label>
-              <input type="text" id="password" name="password" className="border-2 border-black h-11 rounded-lg" />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="password" className="font-semibold">Jenis Kelamin</label>
-              <input type="text" id="password" name="password" className="border-2 border-black h-11 rounded-lg" />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="password" className="font-semibold">Username</label>
-              <input type="text" id="password" name="password" className="border-2 border-black h-11 rounded-lg" />
-            </div>
-            <div className="flex flex-col">
               <label htmlFor="password" className="font-semibold">Password</label>
-              <input type="text" id="password" name="password" className="border-2 border-black h-11 rounded-lg" />
+              <input type="password" id="password" name="password" className="border-2 border-black h-11 rounded-lg" />
             </div>
             <button className="bg-[#5E5A00] text-white h-[50px] rounded-lg text-2xl font-bold">SUBMIT</button>
           </div>
